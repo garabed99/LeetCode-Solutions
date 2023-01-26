49e5b7521daabd527d76cc50c61fbfbd59486680
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2023-01-26 18:45:49 +0400

    updated q1 and pushing q4

--- Solutions/q1_Two_Sum.js
+++ Solutions/q1_Two_Sum.js
@@ -1,4 +1,4 @@
-/*
+/* [EASY]
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
@@ -19,7 +19,7 @@ Input: nums = [3,3], target = 6
 Output: [0,1]
 
 
-Constraints:
+##Constraints:
 
 2 <= nums.length <= 104
 -109 <= nums[i] <= 109
@@ -29,11 +29,6 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 let twoSum = function (nums, target) {
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
